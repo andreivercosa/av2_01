e3278a5f378194de5de4249a3078da49b6e6e363
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-22 20:06:57 -0400

    mostrar valor menor e maior de uma lista

---
+++ avaliacao01_minimomaximolista/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace avaliacao01_minimomaximolista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listanumeros = new List<int>();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write("Informe os numeros d lista: ");
+                listanumeros.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            listanumeros.Sort();//ordenar lista do maior para o menor
+
+            Console.WriteLine("Minimo: {0}", listanumeros[0]);
+            Console.WriteLine("Maximo: {0}", listanumeros[listanumeros.Count- 1]);//Count conta
+        }
+    }
+}
